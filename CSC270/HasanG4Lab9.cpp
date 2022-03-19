74e91782f6c73775976057392f60711216edd98d
fmhasan25 <fmhasan25@gmail.com>
2022-03-19 14:14:25 -0400

    commit

--- CSC270/HasanG4Lab9.cpp
+++ CSC270/HasanG4Lab9.cpp
@@ -1,10 +1,27 @@
-#include<iostream>
+
+/*
+LAB#9 GROUP#4 03/19/2022 MAHAMUD HASAN
+
+ABSTRACT: 
+
+EQUATOIN USED IN THE PROGRAM
+*/
+
+//PRE-PROCESSOR DIRECTIVES
+#include<iostream>///Required for including the libraries to the program
 #include<cmath>
 #include<IOsmanip>
 
-using namespace std;
+using namespace std; //Required for library filenames
 
 int main(){
+//DECLARATION AND INTIALIZATION SECTION
+
+//USER INPUT SECTION
+
+//COMPUTATION- BASIC FORMULAS SECTION
+
+//PRINTED OUTPUT
     cout<<"Hello World!";
     return 0;
 }
