1f63fce726a220bd1e15cb282d4710b28b952570
fmhasan25 <fmhasan25@gmail.com>
2022-03-20 00:06:27 -0400

    is

--- CSC270/whileLoop.cpp
+++ CSC270/whileLoop.cpp
@@ -2,14 +2,19 @@
 /*
 LAB#9 GROUP#4 03/19/2022 MAHAMUD HASAN
 
-ABSTRACT: 
+ABSTRACT: The program will convert USD(user input) into indonesian, angolan and chilean currency. The program will
+also continue to convert currency until input USD amount goes zero. 
 
-EQUATOIN USED IN THE PROGRAM
+EQUATOIN USED IN THE PROGRAM:
+converted_currency = I_multiflied_factor * _amountDollar
+
+E.g.for USD to indonesian Rupia,
+ converted currency (Rupia) = 14342.35 * 100(USD)= 1434235.00
 */
 
 //PRE-PROCESSOR DIRECTIVES
 #include<iostream>///Required for including the libraries to the program
-#include<cmath>
+#include<cmath> 
 #include<stdlib.h>
 #include<iomanip>
 
@@ -25,47 +30,46 @@ cout<<fixed<<setw(10)<<setprecision(2);
 
 //USER INPUT SECTION
 cout<<"Welcome to currency conversion program. Please put the amount"<<endl;
- cin>>_amountDollar;
-
-cout<<"Please provide the currency character."<<endl;
-
-
- cout<<"Please provide the first letter of intended currency"<<endl;//Indonesia, angola, Chile
+cin>>_amountDollar;
+cout<<"Please provide the first letter of intended currency"<<endl;//Indonesia, angola, Chile
+cout<<"For,Indonesian currency\t:I"<<endl;
+cout<<"For,Angolan currency \t:A"<<endl;
+cout<<"For,Chilean currency \t:C"<<endl;
   cin>>intended_currency;
 
-  while (_amountDollar>=0){
+  while (_amountDollar>0){
 
-switch (toupper(intended_currency))
-{   
-    case 'I':    
-     converted_currency = I_multiflied_factor * _amountDollar;
-    cout<<_amountDollar<<" Dollar converting into indonesian Rupiah "<<converted_currency<<endl;
-     cout<<"new amount is "<<_amountDollar;
-    _amountDollar=_amountDollar-10;
-    break;
+    switch (toupper(intended_currency))
+    {   
+      case 'I':  
+      cout<<"new amount is "<<_amountDollar<<" USD";  
+      converted_currency = I_multiflied_factor * _amountDollar;
+      cout<<" ,which is converted into indonesian Rupiah: "<<converted_currency<<endl;
+      _amountDollar=_amountDollar-10; //decrement rate is set 10.
+        break;
 
     case 'A':
+    cout<<"new amount is "<<_amountDollar<<" USD";
     converted_currency = I_multiflied_factor * _amountDollar;
-    cout<<_amountDollar<<" Dollar converting into angolan kwanza "<<converted_currency<<endl;
-      cout<<"new amount is "<<_amountDollar;
-     _amountDollar=_amountDollar-10;
+    cout<<" ,which is converted into angolan kwanza: "<<converted_currency<<endl;
+     _amountDollar=_amountDollar-10;//decrement rate is set 10.
      break;
 
      case 'C':
+    cout<<"new amount is "<<_amountDollar<<" USD";
     converted_currency = I_multiflied_factor * _amountDollar;
-    cout<<_amountDollar<<" Dollar converting into chilean peso "<<converted_currency<<endl;
-     cout<<"new amount is "<<_amountDollar;
-    _amountDollar=_amountDollar-10;
+    cout<<" ,which is converted into chilean peso: "<<converted_currency<<endl;
+    _amountDollar=_amountDollar-10;//decrement rate is set 10.
     break;
 
     default:
     cout<<"The character is wrong"<<endl;
     converted_currency = _amountDollar;
     break;
-  }
+  }//end of switch block
   
          
-    }
+    }//end of the main function
    
     
     
