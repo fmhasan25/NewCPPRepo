726999682c9c893633a5aa0d15a148844c1aa8f5
fmhasan25 <fmhasan25@gmail.com>
2022-03-20 00:13:56 -0400

    With output

--- CSC270/whileLoop.cpp
+++ CSC270/whileLoop.cpp
@@ -14,9 +14,9 @@ E.g.for USD to indonesian Rupia,
 
 //PRE-PROCESSOR DIRECTIVES
 #include<iostream>///Required for including the libraries to the program
-#include<cmath> 
-#include<stdlib.h>
-#include<iomanip>
+#include<cmath> //Required for pow()
+#include<stdlib.h>//Required for toUpper()
+#include<iomanip>//Required for setw(), setprecesion()
 
 using namespace std; //Required for library filenames
 
@@ -29,7 +29,7 @@ char intended_currency;
 cout<<fixed<<setw(10)<<setprecision(2);
 
 //USER INPUT SECTION
-cout<<"Welcome to currency conversion program. Please put the amount"<<endl;
+cout<<"Welcome to currency conversion program. Please put the amount (USD) you want to convert"<<endl;
 cin>>_amountDollar;
 cout<<"Please provide the first letter of intended currency"<<endl;//Indonesia, angola, Chile
 cout<<"For,Indonesian currency\t:I"<<endl;
@@ -43,23 +43,28 @@ cout<<"For,Chilean currency \t:C"<<endl;
     {   
       case 'I':  
       cout<<"new amount is "<<_amountDollar<<" USD";  
+      // computation and basic formula section 
       converted_currency = I_multiflied_factor * _amountDollar;
+      //Printed output
       cout<<" ,which is converted into indonesian Rupiah: "<<converted_currency<<endl;
       _amountDollar=_amountDollar-10; //decrement rate is set 10.
         break;
 
     case 'A':
     cout<<"new amount is "<<_amountDollar<<" USD";
+    // computation and basic formula section 
     converted_currency = I_multiflied_factor * _amountDollar;
     cout<<" ,which is converted into angolan kwanza: "<<converted_currency<<endl;
-     _amountDollar=_amountDollar-10;//decrement rate is set 10.
+     _amountDollar=_amountDollar-20;//decrement rate is set 10.
      break;
 
      case 'C':
     cout<<"new amount is "<<_amountDollar<<" USD";
+    // computation and basic formula section 
     converted_currency = I_multiflied_factor * _amountDollar;
+    //Printed output
     cout<<" ,which is converted into chilean peso: "<<converted_currency<<endl;
-    _amountDollar=_amountDollar-10;//decrement rate is set 10.
+    _amountDollar=_amountDollar-30;//decrement rate is set 10.
     break;
 
     default:
@@ -85,3 +90,63 @@ cout<<"For,Chilean currency \t:C"<<endl;
     // cout<<"The converted amount is :"<<converted_currency<<endl;
     return 0;
 }
+/*
+Welcome to currency conversion program. Please put the amount (USD) you want to convert
+100
+Please provide the first letter of intended currency
+For,Indonesian currency :I
+For,Angolan currency    :A
+For,Chilean currency    :C
+i
+new amount is 100.00 USD ,which is converted into indonesian Rupiah: 1434235.00
+new amount is 90.00 USD ,which is converted into indonesian Rupiah: 1290811.50
+new amount is 80.00 USD ,which is converted into indonesian Rupiah: 1147388.00
+new amount is 70.00 USD ,which is converted into indonesian Rupiah: 1003964.50
+new amount is 60.00 USD ,which is converted into indonesian Rupiah: 860541.00
+new amount is 50.00 USD ,which is converted into indonesian Rupiah: 717117.50
+new amount is 40.00 USD ,which is converted into indonesian Rupiah: 573694.00
+new amount is 30.00 USD ,which is converted into indonesian Rupiah: 430270.50
+new amount is 20.00 USD ,which is converted into indonesian Rupiah: 286847.00
+new amount is 10.00 USD ,which is converted into indonesian Rupiah: 143423.50
+
+Welcome to currency conversion program. Please put the amount (USD) you want to convert
+260
+Please provide the first letter of intended currency
+For,Indonesian currency :I
+For,Angolan currency    :A
+For,Chilean currency    :C
+a
+new amount is 260.00 USD ,which is converted into angolan kwanza: 3729011.00
+new amount is 240.00 USD ,which is converted into angolan kwanza: 3442164.00
+new amount is 220.00 USD ,which is converted into angolan kwanza: 3155317.00
+new amount is 200.00 USD ,which is converted into angolan kwanza: 2868470.00
+new amount is 180.00 USD ,which is converted into angolan kwanza: 2581623.00
+new amount is 160.00 USD ,which is converted into angolan kwanza: 2294776.00
+new amount is 140.00 USD ,which is converted into angolan kwanza: 2007929.00
+new amount is 120.00 USD ,which is converted into angolan kwanza: 1721082.00
+new amount is 100.00 USD ,which is converted into angolan kwanza: 1434235.00
+new amount is 80.00 USD ,which is converted into angolan kwanza: 1147388.00
+new amount is 60.00 USD ,which is converted into angolan kwanza: 860541.00
+new amount is 40.00 USD ,which is converted into angolan kwanza: 573694.00
+new amount is 20.00 USD ,which is converted into angolan kwanza: 286847.00
+
+Welcome to currency conversion program. Please put the amount (USD) you want to convert
+350
+Please provide the first letter of intended currency
+For,Indonesian currency :I
+For,Angolan currency    :A
+For,Chilean currency    :C
+c
+new amount is 350.00 USD ,which is converted into chilean peso: 5019822.50
+new amount is 320.00 USD ,which is converted into chilean peso: 4589552.00
+new amount is 290.00 USD ,which is converted into chilean peso: 4159281.50
+new amount is 260.00 USD ,which is converted into chilean peso: 3729011.00
+new amount is 230.00 USD ,which is converted into chilean peso: 3298740.50
+new amount is 200.00 USD ,which is converted into chilean peso: 2868470.00
+new amount is 170.00 USD ,which is converted into chilean peso: 2438199.50
+new amount is 140.00 USD ,which is converted into chilean peso: 2007929.00
+new amount is 110.00 USD ,which is converted into chilean peso: 1577658.50
+new amount is 80.00 USD ,which is converted into chilean peso: 1147388.00
+new amount is 50.00 USD ,which is converted into chilean peso: 717117.50
+new amount is 20.00 USD ,which is converted into chilean peso: 286847.00
+*/
